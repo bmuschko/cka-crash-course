887c4470fcf1c830ad2d024e98624cc9e37a9748
Benjamin Muschko <benjamin.muschko@gmail.com>
2020-10-29 16:27:41 -0600

    Ensure user can run kubectl

--- exercises/02-install-cluster-kubeadm/solution/master.sh
+++ exercises/02-install-cluster-kubeadm/solution/master.sh
@@ -5,9 +5,9 @@ API_ADV_ADDRESS=$2
 
 kubeadm init --pod-network-cidr $POD_CIDR --apiserver-advertise-address $API_ADV_ADDRESS | tee /vagrant/kubeadm-init.out
 
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown vagrant:vagrant $HOME/.kube/config
+mkdir -p /home/vagrant/.kube
+cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
 
@@ -16,4 +16,3 @@ sed "s+192.168.0.0/16+$POD_CIDR+g" /tmp/calico-default.yaml > /tmp/calico-define
 
 kubectl apply -f /tmp/calico-defined.yaml
 rm /tmp/calico-default.yaml /tmp/calico-defined.yaml
-echo KUBELET_EXTRA_ARGS=--node-ip=10.8.8.10 > /etc/default/kubelet
--- exercises/02-install-cluster-kubeadm/solution/worker.sh
+++ exercises/02-install-cluster-kubeadm/solution/worker.sh
@@ -4,4 +4,3 @@ NODE=$1
 NODE_HOST_IP=20+$NODE
 
 $(cat /vagrant/kubeadm-init.out | grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
-echo KUBELET_EXTRA_ARGS=--node-ip=10.8.8.$NODE_HOST_IP > /etc/default/kubelet
