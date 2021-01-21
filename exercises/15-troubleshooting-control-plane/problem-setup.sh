6fce0ab47ad6cb15f8496eac3855400b24622e93
Benjamin Muschko <benjamin.muschko@gmail.com>
2021-01-21 15:00:51 -0700

    Add comments

--- exercises/15-troubleshooting-control-plane/problem-setup.sh
+++ exercises/15-troubleshooting-control-plane/problem-setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Misconfigures a setting of the Kubernetes scheduler Pod
 sudo sed -i 's/authentication-kubeconfig=\/etc\/kubernetes\/scheduler.conf/authentication-kubeconfig=\/etc\/kubernetes\/scheduler-authentication.conf/g' /etc/kubernetes/manifests/kube-scheduler.yaml
+
+# Schedule a Deployment with three replicas
 kubectl create deployment deploy --image=nginx --replicas=3
