bdb95709d0ccbe96a79fe93fd6970e7bf913cef1
Romain Dauby <romain.dauby@gmail.com>
2022-03-01 22:12:29 -0500

    Use single master.sh vagrant script

---
+++ exercises/04-etcd-backup-restore/etcdctl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install etcdctl
+ETCD_VERSION="v3.4.14"
+
+wget https://github.com/etcd-io/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz -O /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz
+tar xvf /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz -C /tmp
+sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcd /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdctl /usr/local/bin
