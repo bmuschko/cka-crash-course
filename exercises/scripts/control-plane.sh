7d044b977e1f36a5fd2424171ada6beb3d929f00
Romain Dauby <romain.dauby@gmail.com>
2022-05-12 22:04:30 -0400

    Fix nodes internal ip

--- exercises/scripts/control-plane.sh
+++ exercises/scripts/control-plane.sh
@@ -5,12 +5,19 @@ API_ADV_ADDRESS=$2
 
 kubeadm init --pod-network-cidr $POD_CIDR --apiserver-advertise-address $API_ADV_ADDRESS | tee /vagrant/kubeadm-init.out
 
+echo "KUBELET_EXTRA_ARGS=--node-ip=$API_ADV_ADDRESS --cgroup-driver=systemd" > /etc/default/kubelet
+systemctl restart kubelet
+
 mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown vagrant:vagrant /home/vagrant/.kube/config
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
 
-kubectl apply -f https://docs.projectcalico.org/v3.22/manifests/calico.yaml
+kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
+wget https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
+sed -i 's~cidr: 192\.168\.0\.0/16~cidr: 172\.18\.0\.0/16~g' custom-resources.yaml
+kubectl create -f custom-resources.yaml
+rm custom-resources.yaml
 
 cp /etc/kubernetes/admin.conf /vagrant/admin.conf
