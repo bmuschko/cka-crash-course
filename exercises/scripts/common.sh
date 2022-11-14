1de2219fd9ffaf0d065ae4f64848681b8c0a43c7
Marek Brocko <marek.brocko@pan-net.eu>
2022-11-15 00:23:49 +0100

    fix: docker and google apt repos

--- exercises/scripts/common.sh
+++ exercises/scripts/common.sh
@@ -4,26 +4,43 @@ if [ -z ${K8S_VERSION+x} ]; then
   K8S_VERSION=1.23.4-00
 fi
 
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+apt-get update && apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common \
+gnupg \
+lsb-release
+
+# Google Cloud public signing key
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# Add the Kubernetes repository
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Set up docker repository
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 apt-get update
 apt-get install -y \
-  avahi-daemon \
-  libnss-mdns \
-  traceroute \
-  htop \
-  httpie \
-  bash-completion \
-  docker-ce=5:19.03.14~3-0~ubuntu-bionic \
-  kubeadm=$K8S_VERSION \
-  kubelet=$K8S_VERSION \
-  kubectl=$K8S_VERSION
+avahi-daemon \
+libnss-mdns \
+traceroute \
+htop \
+httpie \
+bash-completion \
+docker-ce \
+docker-ce-cli \
+containerd.io \
+docker-compose-plugin \
+kubeadm=$K8S_VERSION \
+kubelet=$K8S_VERSION \
+kubectl=$K8S_VERSION
+
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
