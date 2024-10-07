d519028c7712755250f418c14e83ef35d9fc0ab3
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-10-07 11:30:41 +0200

    Upgrade Ubuntu version

--- exercises/03-etcd-backup-restore/etcdctl.sh
+++ exercises/03-etcd-backup-restore/etcdctl.sh
@@ -5,4 +5,4 @@ ETCD_VERSION="v3.5.12"
 
 wget https://github.com/etcd-io/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz -O /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz
 tar xvf /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz -C /tmp
-sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcd /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdctl /usr/local/bin
+sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcd /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdctl /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdutl /usr/local/bin
